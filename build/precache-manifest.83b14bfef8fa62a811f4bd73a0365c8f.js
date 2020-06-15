b964ff4fe8205a32271ddf5f4e68491150e9f324
wtineomoronta <wtineomoronta@gmail.com>
2020-06-15 11:38:46 -0400

    adding new build

---
+++ build/precache-manifest.83b14bfef8fa62a811f4bd73a0365c8f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdd1ec6195ce6b2e0a71e14730630f90",
+    "url": "/teaching-read/index.html"
+  },
+  {
+    "revision": "de64dd1efc605a1e2a99",
+    "url": "/teaching-read/static/css/main.294ca7c9.chunk.css"
+  },
+  {
+    "revision": "7528ab240a1d2e1ecc2f",
+    "url": "/teaching-read/static/js/2.4af5eba0.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/teaching-read/static/js/2.4af5eba0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de64dd1efc605a1e2a99",
+    "url": "/teaching-read/static/js/main.d8a9b702.chunk.js"
+  },
+  {
+    "revision": "43ed5c3c6445c8203a49",
+    "url": "/teaching-read/static/js/runtime-main.e823638d.js"
+  }
+]);
